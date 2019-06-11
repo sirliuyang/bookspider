d87f940b929b8a1ae90a7215b8698311d3986d8c
Leon <sirliuyang@hotmail.com>
2019-06-11 16:41:57 +0800

    update SMZProcessor

---
+++ src/main/java/com/formula/bookspider/processor/SMZPipeline.java
@@ -0,0 +1,12 @@
+package com.formula.bookspider.processor;
+
+import us.codecraft.webmagic.ResultItems;
+import us.codecraft.webmagic.Task;
+import us.codecraft.webmagic.pipeline.Pipeline;
+
+public class SMZPipeline implements Pipeline {
+    @Override
+    public void process(ResultItems resultItems, Task task) {
+
+    }
+}
