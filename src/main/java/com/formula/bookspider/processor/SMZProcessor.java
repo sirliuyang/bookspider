bdd62300dfdf0577aeabefff0779c5e0d1d9a2f4
Leon <sirliuyang@hotmail.com>
2019-06-11 16:35:00 +0800

    update SMZProcessor

--- src/main/java/com/formula/bookspider/processor/SMZProcessor.java
+++ src/main/java/com/formula/bookspider/processor/SMZProcessor.java
@@ -8,15 +8,27 @@ import java.util.List;
 
 public class SMZProcessor implements PageProcessor {
 
+    public int count = 0;
+    private boolean addTop = false;
+
     private Site site = Site.me().setRetryTimes(5).setSleepTime(2000);
 
     @Override
     public void process(Page page) {
         List<String> products = page.getHtml().xpath("//div[@class='feed-block-ver']").xpath("//h5[@class='feed-ver-title']").links().all();
+        count += products.size();
+        System.out.println(count);
+        /*
         for (String product : products) {
             System.out.println(product);
         }
-        page.addTargetRequests(page.getHtml().xpath("//div[@class='feed-pagenation']").xpath("//div[@class='pagenation-list']").links().all());
+        */
+        if(addTop == false){
+            addTop = true;
+            page.addTargetRequests(page.getHtml().xpath("//div[@id='content']").xpath("//div[@id='filter-block']").xpath("//div[@class='filter-items J_filter_items']").links().all());
+            System.out.println("***********Add top once**********");
+        }
+        page.addTargetRequests(page.getHtml().xpath("//div[@class='feed-pagenation']").xpath("//ul[@class='pagenation-list']").links().all());
     }
 
     @Override
